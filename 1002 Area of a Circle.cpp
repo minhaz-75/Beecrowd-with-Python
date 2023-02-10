467610952d7ffba9561b2d3b9a3eb4b33b6b6faa
Minhazul Islam <110049893+minhaz-75@users.noreply.github.com>
2023-02-10 12:13:38 +0600

    Add files via upload

---
+++ 1002 Area of a Circle.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main()
+{
+    double R, A;
+    cin>>R;
+    const double pi = 3.14159;
+
+    A = pi * R * R;
+
+    cout<< fixed << setprecision(4) << "A=" << A << endl;
+
+    return 0;
+
+
+}
+
